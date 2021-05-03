e523fdf48f515363f35d80984d4d388ca1d48757
pventrella20 <p.ventrella4@studenti.uniba.it>
2021-05-03 12:10:31 +0200

    aggiunto metodo di conversione dati
    - "eliminateGrams()" va ad eliminare la g, mg e mcg e a convertire i rispettivi valori tutti in grammi

--- prova.py
+++ prova.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 
 def parseCSV(path):
+    '''
+    Legge un file .csv e lo incapsula in un dataframe
+    :param path: percorso file .csv
+    :return: dataframe con i dati estratti
+    '''
     column_names = [
         "id",
         "name",
@@ -84,31 +89,40 @@ def parseCSV(path):
     food = pd.read_csv(path, header=None, sep=',', error_bad_lines=False, names=column_names)
     return food
 
+def eliminateGrams(df):
+    '''
+    Elimina grammi, milligrammi e microgrammi dal dataset, convertendo milligrammi e microgrammi in grammi
+    :param df: dataframe su cui effettuare la conversione
+    :return: nuovo dataframe
+    '''
+    for name in df.columns:
+        if name not in accepted:
+            df = df.drop(name, axis=1)
+    for col in convert:
+        for index in df.index:
+            if 'g' in df.loc[index, col]:
+                df.loc[index, col] = df.loc[index, col].replace('g', '')
+            if ' g' in df.loc[index, col]:
+                df.loc[index, col] = df.loc[index, col].replace(' g', '')
+            if 'mg' in df.loc[index, col]:
+                df.loc[index, col] = df.loc[index, col].replace('mg', '')
+                df.loc[index, col] = float(df.loc[index, col]) * 1000
+            if ' mg' in df.loc[index, col]:
+                df.loc[index, col] = df.loc[index, col].replace(' mg', '')
+                df.loc[index, col] = float(df.loc[index, col]) * 1000
+            if 'mcg' in df.loc[index, col]:
+                df.loc[index, col] = df.loc[index, col].replace('mcg', '')
+                df.loc[index, col] = float(df.loc[index, col]) * 1000000
+            if ' mcg' in df.loc[index, col]:
+                df.loc[index, col] = df.loc[index, col].replace(' mcg', '')
+                df.loc[index, col] = float(df.loc[index, col]) * 1000000
+    return df
+
 
 accepted = ["name", "calories", "carbohydrate", "protein", "total_fat"]
 convert = ["carbohydrate", "protein", "total_fat"]
 f_r = parseCSV("nutrition.csv")
-for name in f_r.columns:
-    if name not in accepted:
-        f_r = f_r.drop(name, axis=1)
-for col in convert:
-    for index in f_r.index:
-        if 'g' in f_r.loc[index, col]:
-            f_r.loc[index, col] = f_r.loc[index, col].replace('g', '')
-        if ' g' in f_r.loc[index, col]:
-            f_r.loc[index, col] = f_r.loc[index, col].replace(' g', '')
-        if 'mg' in f_r.loc[index, col]:
-            f_r.loc[index, col] = f_r.loc[index, col].replace('mg', '')
-            f_r.loc[index, col] = float(f_r.loc[index, col]) * 1000
-        if ' mg' in f_r.loc[index, col]:
-            f_r.loc[index, col] = f_r.loc[index, col].replace(' mg', '')
-            f_r.loc[index, col] = float(f_r.loc[index, col]) * 1000
-        if 'mcg' in f_r.loc[index, col]:
-            f_r.loc[index, col] = f_r.loc[index, col].replace('mcg', '')
-            f_r.loc[index, col] = float(f_r.loc[index, col]) * 1000000
-        if ' mcg' in f_r.loc[index, col]:
-            f_r.loc[index, col] = f_r.loc[index, col].replace(' mcg', '')
-            f_r.loc[index, col] = float(f_r.loc[index, col]) * 1000000
+f_r = eliminateGrams(f_r)
 
 # f_r["calories"].hist(bins=15)
 # plt.show()
