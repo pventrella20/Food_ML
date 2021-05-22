347443e512aa6f4882a5a8d0300b9637843a2bc6
pventrella20 <p.ventrella4@studenti.uniba.it>
2021-05-22 23:42:56 +0200

    K-means su nuovo dataset
    - selezionati solo i 'Meals' e provato il k-means su n_cluster = 3

--- prova.py
+++ prova.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+from sklearn import metrics
+from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import MinMaxScaler
 
+
+N_CLUSTER = 3
 
 def parseCSV(path):
     '''
@@ -119,9 +123,99 @@ def splittingAndEval(df):
     print('Predicted food calories for ', example_food, ' is ',
           knn.predict(example_food_scaled)[0] - 1)
 
+def selectMeals(df):
+    df = df.loc[df['Filter group'] == 'Meals']
+    return df
+
+def kMeansCategorization(df, test=False):
+    scaler = MinMaxScaler()
+    scaler.fit(df[convert])
+    df[convert] = scaler.transform(df[convert])
+    kMeans = KMeans(n_clusters=N_CLUSTER)
+    df['cluster'] = kMeans.fit_predict(df[convert])
+
+    # transformToLabel = {0: 'Frutta e Verdura', 1: 'Frutta secca e oli', 2: 'Carne-  e pesce', 3: 'Farinacei', 4: 'Dolci', 5: 'Formaggi'}
+    # prediction = [transformToLabel[pred] for pred in prediction]
+    # df['Cluster_name'] = prediction
+    print(df.head(50))
+
+    if test is True:
+        # hyperparameter evaluation using silhouette score
+        k_to_test = range(2, 25, 1)  # [2,3,4, ..., 24]
+        silhouette_scores = {}
+        for k in k_to_test:
+            kmeans_k = KMeans(n_clusters=k)
+            kmeans_k.fit(df[convert])
+            labels_k = kmeans_k.labels_
+            score_k = metrics.silhouette_score(df[convert], labels_k)
+            silhouette_scores[k] = score_k
+            print("Tested kMeans with k = %d\tSS: %5.4f" % (k, score_k))
+
 
 #accepted = ["name", "calories", "carbohydrate", "protein", "total_fat"]
-#convert = ["carbohydrate", "protein", "total_fat"]
+convert = [ "Energy (kcal)",
+        "Energy (kJ)",
+        "Carbohydrates (g)",
+        "Fat (g)",
+        "Protein (g)",
+        "Fibre (g)",
+        "Water (g)",
+        "Alcohol (g)",
+        "Ash (g)",
+        "Monosaccharides (g)",
+        "Disaccharides (g)",
+        "Sucrose (g)",
+        "Wholegrain total (g)",
+        "Sugar total (g)",
+        "Sum of saturated fatty acids (g)",
+        "Fatty acids 4:0-10:0 (g)",
+        "Fatty acid 12:0 (g)",
+        "Fatty acid 14:0 (g)",
+        "Fatty acid 16:0 (g)",
+        "Fatty acid 18:0 (g)",
+        "Fatty acid 20:0 (g)",
+        "Sum of monounsaturated fatty acids (g)",
+        "Fatty acid 16:1 (g)",
+        "Fatty acid 18:1 (g)",
+        "Sum of polyunsaturated fatty acids (g)",
+        "Fatty acids 18:2 (g)",
+        "Fatty acid 18:3 (g)",
+        "Fatty acid 20:4 (g)",
+        "EPA (Fatty acid 20:5) (g)",
+        "DPA (Fatty acid 22:5) (g)",
+        "DHA (Fatty acid 22:6) (g)",
+        "Cholesterol (mg)",
+        "Retinol (µg)",
+        "Retinolequivalents (RE)",
+        "Beta-carotene (µg)",
+        "Vitamin D (µg)",
+        "Vitamin E (mg)",
+        "Vitamin K (µg)",
+        "Thiamin (mg)",
+        "Riboflavin (mg)",
+        "Vitamin C (mg)",
+        "Niacin (mg)",
+        "Niacin equivalents (NE)",
+        "Vitamin B-6 (mg)",
+        "Vitamin B-12 (µg)",
+        "Folate (µg)",
+        "Phosphorus (mg)",
+        "Iodide (µg)",
+        "Iron (mg)",
+        "Calcium (mg)",
+        "Potassium (mg)",
+        "Magnesium (mg)",
+        "Sodium (mg)",
+        "Salt (g)",
+        "Selenium (µg)",
+        "Zinc (mg)",]
+select = ['Food Name', 'cluster']
 f_r = parseCSV("LivsmedelsDB.csv")
 #f_r = eliminateGrams(f_r)
-splittingAndEval(f_r)
+#splittingAndEval(f_r)
+f_r = selectMeals(f_r)
+print(f_r.isnull().any())
+f_r = f_r.fillna(0)
+kMeansCategorization(f_r)
+s_f = f_r[select]
+print(s_f.head())
