9bd804f7ed6b835489d2f1c81d4ebdb2a5d5c181
pventrella20 <p.ventrella4@studenti.uniba.it>
2021-05-28 21:10:14 +0200

    cross-validation via k-fold
    - implementato k-fold (20 fold) con parameter optimization (neighbors number) -> results best NN = 14
    - caricato plot con matrice di confusione

--- food_project.py
+++ food_project.py
@@ -1,8 +1,33 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sn
+from numpy import mean, std
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, confusion_matrix
+
+
+def cmatrix_display(accuracy, confusion_matrix, names, figsize=(10, 7)):
+    """
+    visualizza a video la matrice di confusione
+    :param spk_names: lista dei parlatori per cui il modello è addestrato
+    :param accuracy: accuracy della predizione
+    :param confusion_matrix_spk: matrice di confusione
+    :param figsize: grandezza dell'immagine
+    :return:
+    """
+    df_cm = pd.DataFrame(confusion_matrix, index=[i for i in names],
+                         columns=[i for i in names])
+    plt.figure(figsize=figsize)
+    plt.title('Confusion Matrix\n Accuracy: {0:.3f}'.format(accuracy))
+    sn.heatmap(df_cm, cmap='rocket_r', annot=True, fmt='g')
+    plt.ylabel('True speaker')
+    plt.xlabel('Predicted speaker')
+    plt.subplots_adjust(bottom=0.155, right=0.924)
+    plt.show()
 
 
 def changeCommas(df):
@@ -19,28 +44,46 @@ def changeCommas(df):
     return df
 
 
+def kFold_cross_validation(X, y, splits=20):
+    kf = KFold(n_splits=splits, random_state=0, shuffle=True)
+    results = []
+    print('Cross validation via K-Fold [{} splits]'.format(splits))
+    for i in range(1, 101):
+        knn_2 = KNeighborsClassifier(n_neighbors=i)
+        scores = cross_val_score(knn_2, X, y, scoring='accuracy', cv=kf, n_jobs=-1)
+        results.append(mean(scores))
+        print('Accuracy: %.3f (%.3f), nn = %d' % (mean(scores), std(scores), i))
+    print('best NN number is {} ({:.2f}%)'.format(results.index(max(results)), max(results)))
+
+
 def knnClassifier(df):
-    food_features = ["carbohydrates_100g", "proteins_100g", "fat_100g", "fiber_100g", "energy_100g"]
+    food_features = ["energy_100g", "carbohydrates_100g", "proteins_100g", "fat_100g", "fiber_100g", "salt_100g",
+                     "sugars_100g", "sodium_100g"]
     X_food = df[food_features]
     y_food = df['nutriscore_grade']
+    # cross-validation
+    kFold_cross_validation(X_food, y_food)
     # preprocessing
-    X_train, X_test, y_train, y_test = train_test_split(X_food, y_food, test_size=0.15, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X_food, y_food, test_size=0.5, random_state=200)
+    # normalizing
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)  # features normalization (train set)
     X_test_scaled = scaler.transform(X_test)  # features normalization (test set)
-    for i in range(1, 100):
-        knn = KNeighborsClassifier(n_neighbors=i)  # creo un knnClassifier
-        knn.fit(X_train_scaled, y_train)  # fit del knn
-        #y_pred = knn.predict(X_test_scaled)
-        print('accuracy for {} neighbors is {:.2f}'.format(i, knn.score(X_test_scaled, y_test)))
-    # print(confusion_matrix(y_test, y_pred))
-    # print(classification_report(y_test, y_pred))
-    # # Checking performance on the training set
-    # print('Accuracy of K-NN classifier on training set: {:.2f}'
-    #       .format(knn.score(X_train_scaled, y_train)))
-    # # Checking performance on the test set
-    # print('Accuracy of K-NN classifier on test set: {:.2f}'
-    #       .format(knn.score(X_test_scaled, y_test)))
+    knn = KNeighborsClassifier(n_neighbors=14)  # creo un knnClassifier
+    knn.fit(X_train_scaled, y_train)  # fit del knn
+    y_pred = knn.predict(X_test_scaled)
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    # Checking performance on the training set
+    print('Accuracy of K-NN classifier on training set: {:.2f}'
+          .format(knn.score(X_train_scaled, y_train)))
+    # Checking performance on the test set
+    print('Accuracy of K-NN classifier on test set: {:.2f}'
+          .format(knn.score(X_test_scaled, y_test)))
+
+    # print confusion matrix
+    names = ["a", "b", "c", "d", "e"]
+    cmatrix_display(knn.score(X_test_scaled, y_test), confusion_matrix(y_test, y_pred), names)
 
 
 def parseCSV(path, separ):
@@ -62,3 +105,4 @@ food_l = ['energy_100g', 'fat_100g', 'saturated-fat_100g', 'monounsaturated-fat_
 food_df = parseCSV("food_dataset2.csv", ',')
 food_df = changeCommas(food_df)
 knnClassifier(food_df)
+
