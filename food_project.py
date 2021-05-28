87000a6a8b0fa4a21dfbece9d981aa54191e25fb
pventrella20 <p.ventrella4@studenti.uniba.it>
2021-05-29 00:11:45 +0200

    hyper_opt, regressor & data_analisys
    - implementata hyperparameter_optimization per knnClassifier
    - implementato regressor (da fixare il k_fold), sembra funzionare meglio del Classifier, ma necessita la conversione dei valori dei nutriscore
    - inserita una visuale dei dati del dataset, settata su 'energy_100g' (da cambiare a piacere)

--- food_project.py
+++ food_project.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sn
 from numpy import mean, std
+from sklearn.feature_selection import RFE
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 
 def cmatrix_display(accuracy, confusion_matrix, names, figsize=(10, 7)):
     """
     visualizza a video la matrice di confusione
-    :param spk_names: lista dei parlatori per cui il modello è addestrato
+    :param names: lista delle categorie di nutriscore (a, b, c, d, e)
     :param accuracy: accuracy della predizione
-    :param confusion_matrix_spk: matrice di confusione
+    :param confusion_matrix: matrice di confusione
     :param figsize: grandezza dell'immagine
     :return:
     """
@@ -29,6 +32,24 @@ def cmatrix_display(accuracy, confusion_matrix, names, figsize=(10, 7)):
     plt.subplots_adjust(bottom=0.155, right=0.924)
     plt.show()
 
+def feature_selection_recursive_elimination(df, n_features):
+    array = df.values
+    X = array[:, 1:29]
+    y = array[:, 1]
+    y = y.astype('int')
+    # feature extraction
+    estimator = SVR(kernel="linear")
+    selector = RFE(estimator, n_features_to_select=n_features, step=1)
+    selector = selector.fit(X, y)
+    print("Num Features: %d" % selector.n_features_)
+    count = 0
+    features_to_select = []
+    for value in selector.support_:
+        if value:
+            features_to_select.append(food_l[count])
+        count += 1
+    print("Selected Features: %s" % features_to_select)
+    print("Feature Ranking: %s" % selector.ranking_)
 
 def changeCommas(df):
     '''
@@ -44,27 +65,34 @@ def changeCommas(df):
     return df
 
 
-def kFold_cross_validation(X, y, splits=20):
-    kf = KFold(n_splits=splits, random_state=0, shuffle=True)
+def kFold_cross_validation(X, y, model_type, splits=20):
+    kf = KFold(n_splits=splits, shuffle=True)
     results = []
     print('Cross validation via K-Fold [{} splits]'.format(splits))
-    for i in range(1, 101):
-        knn_2 = KNeighborsClassifier(n_neighbors=i)
-        scores = cross_val_score(knn_2, X, y, scoring='accuracy', cv=kf, n_jobs=-1)
-        results.append(mean(scores))
-        print('Accuracy: %.3f (%.3f), nn = %d' % (mean(scores), std(scores), i))
+    if model_type == 'classifier':
+        for i in range(1, 101):
+            knn_2 = KNeighborsClassifier(n_neighbors=i)
+            scores = cross_val_score(knn_2, X, y, scoring='accuracy', cv=kf, n_jobs=-1)
+            results.append(mean(scores))
+            print('Accuracy: %.3f (%.3f), nn = %d' % (mean(scores), std(scores), i))
+    if model_type == 'regressor':
+        for i in range(1, 101):
+            knn_2 = KNeighborsRegressor(n_neighbors=i)
+            scores = cross_val_score(knn_2, X, y, scoring='accuracy', cv=kf, n_jobs=-1)
+            results.append(mean(scores))
+            print('Accuracy: %.3f (%.3f), nn = %d' % (mean(scores), std(scores), i))
     print('best NN number is {} ({:.2f}%)'.format(results.index(max(results)), max(results)))
 
 
 def knnClassifier(df):
-    food_features = ["energy_100g", "carbohydrates_100g", "proteins_100g", "fat_100g", "fiber_100g", "salt_100g",
-                     "sugars_100g", "sodium_100g"]
-    X_food = df[food_features]
+    X_food = df[food_l]
     y_food = df['nutriscore_grade']
+    # hyperparameter optimization
+    hyper_opt(X_food, y_food)
     # cross-validation
-    kFold_cross_validation(X_food, y_food)
+    kFold_cross_validation(X_food, y_food, 'classifier', 10)
     # preprocessing
-    X_train, X_test, y_train, y_test = train_test_split(X_food, y_food, test_size=0.5, random_state=200)
+    X_train, X_test, y_train, y_test = train_test_split(X_food, y_food, test_size=0.2, random_state=200)
     # normalizing
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)  # features normalization (train set)
@@ -85,6 +113,66 @@ def knnClassifier(df):
     names = ["a", "b", "c", "d", "e"]
     cmatrix_display(knn.score(X_test_scaled, y_test), confusion_matrix(y_test, y_pred), names)
 
+def knnRegressor(df):
+    X_food = df[food_l]
+    y_food = df['nutriscore_score']
+    # cross-validation
+    kFold_cross_validation(X_food, y_food, 'regressor', 10)
+    X_train, X_test, y_train, y_test = train_test_split(X_food, y_food, test_size=0.1, random_state=200)
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)  # features normalization (train set)
+    X_test_scaled = scaler.transform(X_test)  # features normalization (test set)
+    knn = KNeighborsRegressor(n_neighbors=14)  # creo un knnRegressor
+    knn.fit(X_train_scaled, y_train)  # fit del knn
+    print('Accuracy of K-NN regressor on test set: {:.2f}'
+          .format(knn.score(X_test_scaled, y_test)))
+    acc = knn.score(X_test_scaled, y_test)
+
+    scores = knn.predict(X_test_scaled)
+    # converto i numeri predetti in lettere
+    y_pred = score_converter(scores)
+    y_test_2 = score_converter(y_test)
+    print(confusion_matrix(y_test_2, y_pred))
+    print(classification_report(y_test_2, y_pred))
+    names = ["a", "b", "c", "d", "e"]
+    cmatrix_display(acc, confusion_matrix(y_test_2, y_pred), names)
+
+def hyper_opt(X, y):
+    leaf_size = list(range(1, 50))
+    n_neighbors = list(range(1, 30))
+    p = [1, 2]
+
+    hyperparameters = dict(leaf_size=leaf_size, n_neighbors=n_neighbors, p=p)
+    knn_2 = KNeighborsClassifier()
+    clf = GridSearchCV(knn_2, hyperparameters, cv=10)
+
+    # Fit the model
+    best_model = clf.fit(X, y)
+    # Print The value of best Hyperparameters
+    print('Best leaf_size:', best_model.best_estimator_.get_params()['leaf_size'])
+    print('Best p:', best_model.best_estimator_.get_params()['p'])
+    print('Best n_neighbors:', best_model.best_estimator_.get_params()['n_neighbors'])
+
+def score_converter(y):
+    scores = []
+    for score in y:
+        if score <= -1:
+            scores.append('a')
+        elif score <= 2:
+            scores.append('b')
+        elif score <= 10:
+            scores.append('c')
+        elif score <= 18:
+            scores.append('d')
+        else:
+            scores.append('e')
+    return scores
+
+def data_analisys(df):
+    f, axes = plt.subplots(1, 2, figsize=(20, 4))
+    sn.histplot(data=df['energy_100g'], ax=axes[0])
+    sn.boxplot(data=df['energy_100g'], ax=axes[1])
+    plt.show()
 
 def parseCSV(path, separ):
     '''
@@ -104,5 +192,8 @@ food_l = ['energy_100g', 'fat_100g', 'saturated-fat_100g', 'monounsaturated-fat_
           'phosphorus_100g', 'iron_100g', 'magnesium_100g', 'zinc_100g', 'iodine_100g']
 food_df = parseCSV("food_dataset2.csv", ',')
 food_df = changeCommas(food_df)
+data_analisys(food_df)
+#feature_selection_recursive_elimination(food_df[food_l], 20)
 knnClassifier(food_df)
+#knnRegressor(food_df)
 
